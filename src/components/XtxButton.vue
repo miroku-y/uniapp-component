8cbaa7f8ec3db0345ebee01bd5c6e7bc442b0c57
miroku.yang <miroku.yang@meetsocial.com>
2024-08-09 19:46:25 +0800

    feat:按钮组件

---
+++ src/components/XtxButton.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: miroku.yang
+ * @Date: 2024-08-06 16:39:59
+ * @LastEditors: Please set LastEditors
+ * @Description: 
+-->
+<template>
+    <view class="uui-button-wrap">
+      <XtxButton class="uui-button" :class="[`uui-button--${type}`]"></XtxButton>
+    </view>
+  </template>
+  
+  <script lang="ts">
+  // 定义 props 接收
+  defineProps<{
+    type: string;
+  }>(),{
+      type:'default'
+  };
+  </script>
+  
+  <style lang="scss">
+    @import './styles/button.scss'
+  </style>
+  
---
+++ src/pages/button/index.vue
@@ -0,0 +1,10 @@
+<!--
+ * @Author: miroku.yang
+ * @Date: 2024-07-31 20:21:04
+ * @LastEditors: Please set LastEditors
+ * @Description: 
+-->
+<template>
+    <view>按钮类型</view>
+    <Button>默认按钮</Button>
+</template>
